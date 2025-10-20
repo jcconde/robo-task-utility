8babb064865dc607f9b4761bee96511ceef1357a
Juanca <juancarlosc@onetree.com>
2025-10-20 11:36:08 -0400

    Add HelloWorldCommand with a basic hello world function

---
+++ src/Commands/HelloWorldCommand.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @copyright 2025 Onetree. All rights reserved.
+ * @author    Juanca <juancarlosc@onetree.com>
+ */
+
+declare(strict_types=1);
+
+namespace Juanca\RoboTaskUtility\Commands;
+
+use Robo\Symfony\ConsoleIO;
+use Robo\Tasks;
+
+/**
+ * @SuppressWarnings("PHPMD.ShortVariable")
+ */
+class HelloWorldCommand extends Tasks
+{
+    /**
+     * @param ConsoleIO $io
+     * @return void
+     */
+    public function hello(ConsoleIO $io): void
+    {
+        $io->say("Hello world");
+    }
+}
